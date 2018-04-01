7a179a160d2858fe409b32601272b83166073632
Karen Galoyan <karengaloian@gmail.com>
2018-04-01 14:56:29 +0400

    Added textLabel for buttonAction

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var textLabel: UILabel!
+    
+    // MARK: - UIViewController Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Actions
